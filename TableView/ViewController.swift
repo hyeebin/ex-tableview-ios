717e0f3ee681465e221e9b1f1b43d0a5817fd6f2
hyebin <developerhyebin@gmail.com>
2023-10-09 23:31:42 +0900

    base view 생성

--- TableView/ViewController.swift
+++ TableView/ViewController.swift
@@ -9,6 +9,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // 가로 스크롤 테이블 뷰 영역
+    @IBOutlet weak var verticalView: UIView!
+    // 새로 스크롤 테이블 뷰 영역
+    @IBOutlet weak var horizonView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
